4745d0758dffa3d7a7e9578ed289ff81e734c7a6
Rushikesh Ghuge <rushikeshghuge993@gmail.com>
2024-09-05 09:20:25 +0530

    post

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     origin: 'http://localhost:4200', // Replace with your frontend URL
     credentials: true,
   });
-  await app.listen(process.env.PORT, '0.0.0.0.');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0.');
 }
 bootstrap();
