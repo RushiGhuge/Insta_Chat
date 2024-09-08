f287d0739cc9d9998ade7b87f4a881204df093f1
Rushikesh Ghuge <rushikeshghuge993@gmail.com>
2024-09-08 08:40:43 +0530

    checks

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,9 +16,9 @@ async function bootstrap() {
   );
   // Enable CORS with default options
   app.enableCors({
-    origin: '*', // Replace with your frontend URL
+    origin: ['http://localhost:4200', 'https://insta-chat-six.vercel.app'], // Replace with your frontend URL
     methods: '*',
-    allowedHeaders: '*',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
