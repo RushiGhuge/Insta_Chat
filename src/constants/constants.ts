3316d2fc794225a8b88994f5785c2de395f32107
Rushikesh Ghuge <rushikeshghuge993@gmail.com>
2024-09-24 15:34:41 +0530

    done

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const CHAT_BACKEND_URL = 'https://chat-backend-ogoo.onrender.com';
-// export const CHAT_BACKEND_URL = 'http://localhost:3000';
+// export const CHAT_BACKEND_URL = 'https://chat-backend-ogoo.onrender.com';
+export const CHAT_BACKEND_URL = 'http://localhost:3000';
